6e772c9e3b12be43028be1f0d8f33a53e63e30ac
BiuBiu <tieutruongkhanh@gmail.com>
2026-02-10 22:50:53 +0700

    Refactor CORS configuration to use only the frontend domain

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,10 +16,7 @@ app.use(express.urlencoded());
 app.use(cookieParser());
 app.use(
   cors({
-    origin:
-      process.env.MODE === "PRODUCTION"
-        ? process.env.FRONTEND_DOMAIN
-        : "http://localhost:5173",
+    origin: process.env.FRONTEND_DOMAIN,
     credentials: true,
   }),
 );
