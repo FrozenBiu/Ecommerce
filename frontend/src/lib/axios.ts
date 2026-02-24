36e1c340940846a38c4fe16a8986f4964b86d521
BiuBiu <tieutruongkhanh@gmail.com>
2026-02-24 20:53:24 +0700

    Update .env variable: MODE to VITE_MODE

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    import.meta.env.MODE === "PRODUCTION"
+    import.meta.env.VITE_MODE === "PRODUCTION"
       ? import.meta.env.VITE_API_URL
       : "http://localhost:5001/api/",
   withCredentials: true,
